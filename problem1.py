d57f741dd47e2049ca76c92e5f3381f926f6006d
Andy <avo6@outlook.com>
2019-05-04 13:01:00 -0700

    First commit for problem 1.

---
+++ problem1.py
@@ -0,0 +1,7 @@
+def multiples_of_3_5():
+    for number in range(1000):
+        #print (number)
+        if (number % 3) == 0  or (number % 5) == 0:
+                yield (number)
+
+print(sum(multiples_of_3_5()))
